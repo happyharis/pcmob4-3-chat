33c39ebdf80af4e8e5bb1e8f93d00149969014b8
Haris Samingan <muhamad_haris@hotmail.com>
2022-06-16 10:55:14 +0800

    Add login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,26 +1,80 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState } from "react";
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <View>
-      <Text>Email</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Chat App</Text>
+      <Text style={styles.fieldTitle}>Email</Text>
       <TextInput
-        placeholder="Enter email"
-        onChangeText={(text) => setEmail(text)}
+        style={styles.input}
+        placeholder="Enter Email"
         value={email}
+        onChangeText={(text) => setEmail(text)}
       />
-      <Text>Password</Text>
+      <Text style={styles.fieldTitle}>Password</Text>
       <TextInput
-        placeholder="Enter password"
+        style={styles.input}
+        placeholder="Enter Password"
         secureTextEntry={true}
-        onChangeText={(text) => setPassword(text)}
         value={password}
+        onChangeText={(text) => setPassword(text)}
       />
+
+      <TouchableOpacity style={styles.loginButton} onPress={null}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  errorText: {
+    color: "red",
+    marginVertical: 20,
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 24,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  fieldTitle: {
+    fontSize: 18,
+    marginBottom: 12,
+  },
+  input: {
+    borderColor: "#999",
+    borderWidth: 1,
+    marginBottom: 24,
+    padding: 4,
+    height: 36,
+    fontSize: 18,
+    backgroundColor: "white",
+  },
+  loginButton: {
+    backgroundColor: "blue",
+    width: 120,
+    alignItems: "center",
+    padding: 18,
+    marginTop: 12,
+    marginBottom: 36,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
